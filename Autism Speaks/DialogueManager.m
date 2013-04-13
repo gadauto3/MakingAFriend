8bd408a032618a39a9057f87f62c790ca78222a2
jayzalowitz <jayzalowitz@gmail.com>
2013-04-13 13:50:16 -0700

    adding dialog options

--- Autism Speaks/DialogueManager.m
+++ Autism Speaks/DialogueManager.m
@@ -11,6 +11,7 @@
 @interface DialogueManager ()
 
 @property (nonatomic, strong) NSMutableArray *allDialogue;
+@property (nonatomic, strong) NSMutableArray *dialogueOptions;
 
 @end
 
@@ -23,12 +24,48 @@
     NSString * b = @"bar";
     NSString * z = @"baz";
     self.allDialogue = [NSMutableArray arrayWithObjects:f,b,z,nil];
-    return self.allDialogueForBoy;
+    return self.allDialogue;
 }
 
 - (NSArray *)randomOptionsForBoy:(int)numToGet
 {
-//	self.allDialogueForBoy
+    self.dialogueOptions = [NSMutableArray array];
+    
+    
+    
+    
+    
+    NSMutableArray *shuffled = [NSMutableArray array];
+
+   
+    NSUInteger arcount = [_allDialogue count]; // gives a random array
+    //return count;
+    if (arcount > 0) {
+        [shuffled addObject:[self.dialogueOptions objectAtIndex:0]];
+        
+        NSUInteger j;
+        for (NSUInteger i = 1; i < arcount; ++i) {
+            j = arc4random() % i; 
+            [shuffled addObject:[self.dialogueOptions objectAtIndex:j]];
+            [shuffled replaceObjectAtIndex:j
+                                withObject:[self.dialogueOptions objectAtIndex:i]];
+        }
+        
+        
+        
+        //      return shuffled; // still autoreleased
+    }
+    
+    // numbtoget specifys the number to be in final array, must generate
+    if (numToGet != 0){
+        for (int count = 0; count <= numToGet; count++) {
+            
+        // pass that array index into array for output
+        [self.dialogueOptions insertObject:[shuffled objectAtIndex:count] atIndex:count];
+        }
+    }
+    
+    return self.dialogueOptions;
 }
 
 - (NSArray *)selectedDialogueByBoy
