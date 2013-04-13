6bf0226182e8ff87d30ac31b1e78256202c67b27
Gabriel Adauto <gabriel@motionmathgames.com>
2013-04-13 03:26:16 -0700

    Placeholder class for characters

---
+++ Autism Speaks/CharacterVC.h
@@ -0,0 +1,13 @@
+//
+//  CharacterVC.h
+//  Autism Speaks
+//
+//  Created by Gabriel Adauto on 4/13/13.
+//  Copyright (c) 2013 Motion Math. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterVC : UIViewController
+
+@end
